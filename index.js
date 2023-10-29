7e72e16a230d946be91e68597647ff7a7b756905
KC-Nick <wilson.kasinichole@gmail.com>
2023-10-29 18:26:18 -0400

    added comments in shapes and index to keep track of what code is doing what

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const inquirer = require('inquirer');
 const {Circle, Triangle, Square} = require('./lib/shapes');
 const fs = require('fs');
 const path = require('path');
+//Validates the text input by inquirer
+const logoTextValidator = async (input) => {
+    if (input.length > 3) {
+       throw new Error ('Please no more than 3 characters.');
+    }
+    return true;
+ };
 
 async function mainMenu () {
+    //awaits the prompts to be answered before the rest of the code runs
     const response = await inquirer.prompt([
         {
             name: 'text',
             type: 'input', 
-            message: 'Enter text for your logo.'
+            message: 'Enter text for your logo.',
+            validate: logoTextValidator()
         },
         {
             name: 'textColor',
@@ -27,6 +36,7 @@ async function mainMenu () {
             message: 'What color would you like the background of your logo?'
         }
     ]);
+    //defines shape
     let shape;
     if (response.shape === 'Circle') {
         shape = new Circle;
@@ -37,6 +47,7 @@ async function mainMenu () {
     if (response.shape === 'Triangle') {
         shape = new Triangle;
     }
+    //defines shape's color based on response
     shape.setColor(response.bgColor);
     let svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
@@ -45,6 +56,7 @@ async function mainMenu () {
     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
   
   </svg>`
+  //accesses file path using __dirname to create file in specific location
   fs.writeFileSync(path.join(__dirname, './examples/logo.svg'), svg);
 }
 mainMenu();
